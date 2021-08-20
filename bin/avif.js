123fdc6f1489d6808ef1d5afa412af7a186ddd2c
Lovell Fuller <github@lovell.info>
2021-08-20 09:36:05 +0100

    Bump sharp, add chromaSubsampling option

--- bin/avif.js
+++ bin/avif.js
@@ -6,6 +6,7 @@ const {
   lossless,
   quality,
   speed,
+  chromaSubsampling,
   overwrite,
   verbose,
 } = require("../lib/cli");
@@ -25,6 +26,7 @@ const avif = async () => {
         lossless,
         quality,
         speed,
+        chromaSubsampling,
         overwrite,
         verbose,
       })
