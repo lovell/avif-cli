ad79b1a38039795716dd5fdc559ac19fea84b06e
Kid <44045911+kidonng@users.noreply.github.com>
2022-06-10 03:58:14 +0800

    Create output directory if not exist (#10)

--- bin/avif.js
+++ bin/avif.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const fs = require("fs").promises;
+const { constants } = require("fs");
 const {
   input,
   output,
@@ -18,6 +20,13 @@ const avif = async () => {
   const files = await glob(input);
   if (verbose) {
     process.stdout.write(`Found ${files.length} file(s) matching ${input}\n`);
+  }
+  if (output) {
+    try {
+        await fs.access(output, constants.W_OK);
+    } catch (e) {
+        await fs.mkdir(output, { recursive: true });
+    }
   }
   const results = await Promise.all(
     files.map((file) =>
